cecc72a790cece7532a378e2eca3742dbb02b3fb
Bruno Salmon <dev.salmonb@gmail.com>
2022-10-31 12:00:58 +0000

    Added Gluon module

---
+++ webfx-demo-demofx-application-gluon/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.demofx.application.gluon {
+
+    // Direct dependencies modules
+    requires javafx.media;
+    requires webfx.demo.demofx.application;
+    requires webfx.kit.openjfx;
+    requires webfx.platform.boot.java;
+    requires webfx.platform.console.java;
+    requires webfx.platform.resource.java;
+    requires webfx.platform.scheduler.java;
+    requires webfx.platform.shutdown.java;
+
+}
